98f2e98d76a99651ca34d474fb223d5b332cbeb5
Andreas Lind <andreaslindpetersen@gmail.com>
2019-07-21 20:48:05 +0200

    Fix duplicated test

--- test/index.spec.js
+++ test/index.spec.js
@@ -11,10 +11,6 @@ describe('ander', function() {
     expect(ander(['foo', 'bar', 'quux'], 'da'), 'to equal', 'foo, bar og quux');
   });
 
-  it('should render a list in Danish', function() {
-    expect(ander(['foo', 'bar', 'quux'], 'da'), 'to equal', 'foo, bar og quux');
-  });
-
   it('should support dashes in locale ids', function() {
     expect(
       ander(['foo', 'bar', 'quux'], 'en-US'),
